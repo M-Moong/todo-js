43e11dce2d8c64b8c0c2aef77339e1a7a610e3d0
Moong <tmdals9154@gmail.com>
2023-08-26 13:48:39 +0900

    aaaa

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import jsConfigPath from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/todo-js/",
   plugins: [react(), jsConfigPath()],
   server: {
     port: 4000
